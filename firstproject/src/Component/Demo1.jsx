4e44619789bf22c6c1783d163cdd069ea92dc53a
Himanshu-01-4B9 <himanshugit01@gmail.com>
2026-03-10 07:26:43 +0000

    this is the second day

---
+++ firstproject/src/Component/Demo1.jsx
@@ -0,0 +1,7 @@
+function Demo1()
+{
+    return(
+        <h1> This is an example of component</h1>
+    )
+}
+export default Demo1
---
+++ firstproject/src/Component/Demo2.jsx
@@ -0,0 +1,7 @@
+function Demo2()
+{
+    return(
+        <h1>this is the 2nd example of component</h1>
+    )
+}
+export default Demo2
